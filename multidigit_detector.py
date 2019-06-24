d586a9a0ae7cd0c508e99df248b7bd36fe3714ed
juan <21977733+juanmed@users.noreply.github.com>
2019-06-24 13:02:57 +0900

    Features
    - Se modifico la forma en que se guarda el model para posterior uso
    - Se anadio detector de numeros multidigitos y codigo para entrenar
    usando el dataset SVHN

--- multidigit_detector.py
+++ multidigit_detector.py
@@ -21,19 +21,10 @@ if not os.path.exists(model_save_path):
     os.mkdir(model_save_path)
 
 
-classes = {'vam':21, 'tod':3, 'une':6, 'uni':7, 'con':13,
-           'cre':14, 'fcn':12, 'win':16, 'ppt': 19, 'unid': 18,
-           'eg': 10, 'urn':5, 'vic':15, 'phg':23, 'viv':11,
-           'ava':22, 'lib':27, 'pan':1, 'mlp': 24, 'val':2, 'vali':40,
-           'nul':41, 'bla':42, 'vale': 43, 'inv': 44, 'fue':17,
-           'ucn': 9,  'pc':25, 'sem':26, 'pod':4, 'bie':8 }
 
-classes = {'vam':0, 'tod':1, 'une':2, 'uni':3, 'con':4,
-           'cre':5, 'fcn':6, 'win':7, 'ppt': 8, 'unid': 9,
-           'eg': 10, 'urn':11, 'vic':12, 'phg':13, 'viv':14,
-           'ava':15, 'lib':16, 'pan':17, 'mlp': 18, 'val':19, 'vali':20,
-           'nul':21, 'bla':22, 'vale': 23, 'inv': 24, 'fue':25,
-           'ucn': 26,  'pc':27, 'sem':28, 'pod':29, 'bie':30 }
+
+classes = {'10':0, '1':1, '2':2, '3':3, '4':4,
+           '5':5, '6':6, '7':7, '8': 8, '9': 9}
 
 num_classes = len(classes)
 print(">> Total classes: {}".format(num_classes))
@@ -43,12 +34,11 @@ num_epochs = 10
 learning_rate = 0.005
 momentum = 0.5
 weight_decay = 0.005
-save_model = True
+save_model = False
 save_frequency = 2
 
-batch_size_train = 2
-batch_size_test = 5
-batch_size_val = 5
+batch_size_train = 8
+batch_size_test = 8
 
 img_width = 1634
 img_height = 2182
@@ -80,6 +70,7 @@ class ActasLoader(torch.utils.data.Dataset):
 
         self.root = os.path.expanduser(root)
         self.transform = transform
+        self.target_transform = target_transform
         self.split = split
 
         self.train_dir = os.listdir(self.root + 'train/')
@@ -107,7 +98,7 @@ class ActasLoader(torch.utils.data.Dataset):
         #print(img.shape)
         #img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
         #img = Image.fromarray(img, mode='RGB')
-        target_dict = self.get_labels(img_path.replace('.jpg','.xml'))
+        target_dict = self.get_labels(img_path.replace('.png','.xml'))
         target_dict['image_id'] = torch.tensor([index])
 
         boxes = target_dict['boxes']
@@ -119,9 +110,12 @@ class ActasLoader(torch.utils.data.Dataset):
         target_dict["iscrowd"] = iscrowd
 
         if self.transform is not None:
-            img, target = self.transform(img, target_dict)
+            img = self.transform(img)
 
-        return img, target
+        if self.target_transform is not None:
+            target_dict = self.target_transform(target_dict)
+
+        return img, target_dict
 
     def __len__(self):
         """
@@ -147,10 +141,10 @@ class ActasLoader(torch.utils.data.Dataset):
             labels.append(lbl)
 
             # get bounding box
-            xmin =int(member[4][0].text)#/width
-            ymin = int(member[4][1].text)#/height
-            xmax = int(member[4][2].text)#/width
-            ymax = int(member[4][3].text)#/height
+            xmin =int(member[1][0].text)#/width
+            ymin = int(member[1][1].text)#/height
+            xmax = int(member[1][2].text)#/width
+            ymax = int(member[1][3].text)#/height
             bbox = [xmin, ymin, xmax, ymax]
             bboxes.append(bbox)
 
@@ -163,47 +157,6 @@ class ActasLoader(torch.utils.data.Dataset):
         target_dict['labels'] = torch.as_tensor(labels, dtype=torch.int64)
 
         return target_dict
-
-def eval(model, data_loader, device):
-    n_threads = torch.get_num_threads()
-    # FIXME remove this and make paste_masks_in_image run on the GPU
-    torch.set_num_threads(1)
-    cpu_device = torch.device("cpu")
-    model.eval()
-    metric_logger = utils.MetricLogger(delimiter="  ")
-    header = 'Test:'
-
-    coco = get_coco_api_from_dataset(data_loader.dataset)
-    iou_types = _get_iou_types(model)
-    coco_evaluator = CocoEvaluator(coco, iou_types)
-
-    for image, targets in metric_logger.log_every(data_loader, 100, header):
-        image = list(img.to(device) for img in image)
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-
-        torch.cuda.synchronize()
-        model_time = time.time()
-        outputs1 = model(image)
-
-        outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs1]
-        model_time = time.time() - model_time
-
-        res = {target["image_id"].item(): output for target, output in zip(targets, outputs)}
-        evaluator_time = time.time()
-        coco_evaluator.update(res)
-        evaluator_time = time.time() - evaluator_time
-        metric_logger.update(model_time=model_time, evaluator_time=evaluator_time)
-
-    # gather the stats from all processes
-    metric_logger.synchronize_between_processes()
-    print("Averaged stats:", metric_logger)
-    coco_evaluator.synchronize_between_processes()
-
-    # accumulate predictions from all images
-    coco_evaluator.accumulate()
-    coco_evaluator.summarize()
-    torch.set_num_threads(n_threads)
-    return coco_evaluator, outputs1
 
 def main():
 
@@ -232,13 +185,13 @@ def main():
     do_learn = True
     if(do_learn):
         # load datasets
-        #trans = transforms.Compose([ transforms.Resize((img_height, img_width)), transforms.ToTensor()])
-        trans = get_transform()
-        train_dataset = ActasLoader(root = './actas_dataset/', split='train', transform = trans)
-        test_dataset = ActasLoader(root = './actas_dataset/', split='test', transform = trans)
+        trans = transforms.Compose([transforms.ToTensor()]) #transforms.Resize((img_height, img_width)),
+        #trans = get_transform()
+        train_dataset = ActasLoader(root = './svhn_dataset/', split='train', transform = trans)
+        test_dataset = ActasLoader(root = './svhn_dataset/', split='test', transform = trans)
         
-        for item in train_dataset:
-            pass
+        #for item in train_dataset:
+        #    pass
 
         train_loader = torch.utils.data.DataLoader( train_dataset, batch_size = batch_size_train, shuffle = True, collate_fn=utils.collate_fn, num_workers=4)
         test_loader = torch.utils.data.DataLoader( test_dataset, batch_size = batch_size_test, shuffle = True, collate_fn=utils.collate_fn, num_workers=4)
@@ -246,30 +199,40 @@ def main():
         for epoch in range(num_epochs):
 
             train_one_epoch(model, optimizer, train_loader, device, epoch, print_freq=1)
-            #lr_scheduler.step()  # refrescar taza de aprendizaje
+            lr_scheduler.step()  # refrescar taza de aprendizaje
             evaluate(model, test_loader, device=device)
 
             if save_model and ((epoch % save_frequency) == 0) and epoch != 0 :
-                torch.save(model, model_save_path+'detector_totales_nn_{:03}.pt'.format(epoch))
-                print(">> Saving model: {}".format(model_save_path+'detector_totales_nn_{:03}.pt'.format(epoch)))
+                torch.save(modelo.state_dict(), model_save_path+'mnist_nn_{:03}.pt'.format(epoch))
+                print(">> Saving model: {}".format(model_save_path+'mnist_nn_{:03}.pt'.format(epoch)))
             else:
                 # not moment to save
                 pass
 
+        #img, target = train_dataset[0]
+        #for box, lbl in zip(target['boxes'], target['labels']):
+        #    print(box,lbl)
     else:
-        # load eval data
-        trans = get_transform()
-        val_dataset = ActasLoader(root = './actas_dataset/', split='test', transform = trans)        
-        val_loader = torch.utils.data.DataLoader( val_dataset, batch_size = batch_size_val, shuffle = True, collate_fn=utils.collate_fn, num_workers=4)
-        
-        model=torch.load(model_save_path+"detector_totales_nn_200.pt")    
-        #model.load_state_dict(torch.load(model_save_path+"detector_totales_nn_200.pt"))
-        coco_eval, output = eval(model, val_loader, device)
-        print(coco_eval)
-        print(output)
+        pass
 
 
+    #plt.imshow(img)
+    #plt.show()
 
+    
+    """
+    class_id = []
+    for key in classes.keys():
+        class_id.append(classes[key])
+
+    a = np.arange(30)
+    print (a)
+    for id in a:
+        if id not in class_id:
+            print(id)
+        else:
+            pass
+    """
 
 if __name__ == '__main__':
     main()
